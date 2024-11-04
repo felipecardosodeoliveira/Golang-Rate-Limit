7a79d43285610425c6eca4f05940faa6005b4dd8
felipecardosodeoliveira <felipecardosodeoliveira02@gmail.com>
2024-11-04 00:04:19 -0300

    feat(main): Desafio golang

---
+++ Readme.md
@@ -0,0 +1,48 @@
+# Golang Rate Limiter
+
+Este projeto é uma implementação de um limitador de taxa em Go, utilizando Redis como armazenamento. O limitador permite limitar requisições por IP e por token.
+
+
+## Instalação
+
+1. **Clone o repositório**:
+   ```bash
+   git clone https://github.com/seu_usuario/golang-rate-limit.git
+   cd golang-rate-limit
+
+2. **Váriaveis de ambiente**
+   RATE_LIMIT_IP=1 (limite por ip)
+   RATE_LIMIT_TOKEN=2 (limite por token)
+   BLOCK_TIME=5   (tempo de bloqueio quando o limite for atingide)
+   REDIS_HOST=redis (redis)
+   REDIS_PORT=6379  (porta)
+
+3. **Build o projeto**:
+   ```bash
+      docker compose up --build
+
+4. **Fazendo chamadas http**:
+
+Dentro da pasta doc há um arquivo que pose ser executado para fazer as chamadas por ip ou com o
+toke no header.
+
+5. **Testes**:
+   ```bash
+   Teste com curl
+   Para testar o limite de IP:
+ 
+   Copiar código
+   ```bash
+   for i in {1..5}; do curl -i http://localhost:8080/; done
+
+
+   Para testar com um token:
+   Copiar código
+   for i in {1..5}; do curl -i -H "API_KEY: teste_123" http://localhost:8080/; done  
+
+   Para testar com o Apache Bench:
+   ```bash
+   Copiar código
+   ab -n 10 -c 1 http://localhost:8080/
+
+
---
+++ test/limiter_test.go
@@ -0,0 +1,55 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github/felipecardosodeoliveira/golang-rate-limit/internal/limiter"
+	"github/felipecardosodeoliveira/golang-rate-limit/internal/middleware"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRateLimiter(t *testing.T) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	store := limiter.NewRedisStore(rdb)
+
+	rl := limiter.NewRateLimiter(store, 5, 10, 10)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	limitedHandler := middleware.RateLimiter(rl, handler)
+
+	for i := 0; i < 5; i++ {
+		req := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+		req.RemoteAddr = "192.168.1.1"
+		resp := httptest.NewRecorder()
+
+		limitedHandler.ServeHTTP(resp, req)
+
+		assert.Equal(t, http.StatusOK, resp.Code)
+	}
+
+	req := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	req.RemoteAddr = "192.168.1.1"
+	resp := httptest.NewRecorder()
+
+	limitedHandler.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusTooManyRequests, resp.Code)
+
+	time.Sleep(11 * time.Second)
+
+	req = httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	req.RemoteAddr = "192.168.1.1"
+	resp = httptest.NewRecorder()
+
+	limitedHandler.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code)
+}
